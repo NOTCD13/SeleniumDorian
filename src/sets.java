0a330b796d7ac1717b63e0744bc405f0bb26c032
NOTCD13 <doriancamp13@gmail.com>
2025-06-02 11:45:15 -0600

    video 33

---
+++ src/sets.java
@@ -0,0 +1,21 @@
+public class sets {
+    public static void main(String[] args) {
+        /*
+        * Declarar Sets
+        * final var setNumeros = new HashSet<>(Arrays.asList(3,4,5,6,7,));
+        * final var setNombres = new HashSet<>(Arrays.asList("Carlos","Carlos","Carlos","Carlos"));
+        *
+        * final var setNumeros = new HashSet<Integer>();
+        * final var setNombres = new HashSet<String>();
+        *
+        * metodo add
+        * imprimir con un %s
+        * metodo size()
+        * iterar set con foreach
+        *
+        * set en funciones
+        * como parametro: static void imprimirSetNombre(Set<String> setNombres){}
+        * como tipo de retorno: static Set<String> crearSetNombres(){}
+        * */
+    }
+}
